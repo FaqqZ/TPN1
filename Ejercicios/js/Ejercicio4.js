f5fb506a20251aa22b5e67b58b99ede1c71888cf
Facundo <facundo4349674@gmail.com>
2023-11-13 21:02:26 -0300

    Ejercicios del 1 al 5

---
+++ Ejercicios/js/Ejercicio4.js
@@ -0,0 +1,2 @@
+let nombre = prompt('Hola ingresa tu nombre de usuario:')
+document.write("Hola " + nombre + ' ¿Cómo estás?')
---
+++ Ejercicios/js/Ejercicio5.js
@@ -0,0 +1,3 @@
+let valor1 = parseInt(prompt('Ingresa el primer valor de la suma: '))
+let valor2 =  parseInt(prompt('Ingresa el segundo valor de la suma:'))
+alert((valor1+valor2))
